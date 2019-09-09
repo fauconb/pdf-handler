b1d77f38353719985100d4a9afba73ab5e7992cd
Sik4 <cam.maury@gmail.com>
2019-09-09 12:33:23 +0200

    created components, using ngx-extended-pdf-viewer

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgxExtendedPdfViewerModule} from "ngx-extended-pdf-viewer";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ViewerComponent } from './viewer/viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ViewerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxExtendedPdfViewerModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
