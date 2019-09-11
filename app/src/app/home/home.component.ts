f4fd58fa7338b9127ec3af74ea69fc4b9fb4798c
Sik4 <cam.maury@gmail.com>
2019-09-11 10:41:11 +0200

    Viewer working with API, CSS mod (footer)

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {  FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
 
-const URL = 'assets/pdf/';
+const URL = 'http://localhost:8080/api-pdf-handler/resources/upload/';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +13,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
     this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
-      console.log('ImageUpload:uploaded:', item, status, response);
+      console.log('pdfUpload:uploaded:', item, status, response);
       alert('File uploaded successfully');
     };
   }
--- app/src/app/viewer/viewer.component.ts
+++ app/src/app/viewer/viewer.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+let url = "http://localhost:8080/api-pdf-handler/resources/pdf/view?name=pdf-sample.pdf"
+
 @Component({
   selector: 'app-viewer',
   templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.css']
 })
+
+
 export class ViewerComponent implements OnInit {
 
   constructor() { }
